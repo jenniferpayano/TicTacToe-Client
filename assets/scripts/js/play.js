7fbd8fcfc0ab621a9a20879d62873e0f823f2be5
jenniferpayano <arlenepayano@gmail.com>
2020-03-11 16:35:44 -0400

    Empty array to store players play.
    this was added to play.js

--- assets/scripts/js/play.js
+++ assets/scripts/js/play.js
@@ -1,14 +1,14 @@
 const winningPlays = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [6, 4, 2]]
+const playerX = ['', '', '', '', '', '', '', '', '']
 
 function playedDiv (x) {
-  playerX = []
-  playerY = []
-  let xValue = parseInt(x)
+  const xValue = parseInt(x)
   console.log('User played div ' + x)
-  if(playerX.length <= 3) {
-    playerX.push(xValue)
-    console.log(xValue)
-    console.log(playerX.length)
-  }
-
+  playerX[xValue] = xValue
+  console.log(xValue)
+  console.log(playerX)
+  // console.log(playerX.length)
+  // for (let i =3 ; i <= 9 ; i++)
+    // checkgame = checkLine()
+    // return checkline
 }
