783f3bbb866de3502dd34f1be080b0c07666d3b0
jenniferpayano <arlenepayano@gmail.com>
2020-03-12 11:40:04 -0400

    Added clearBoard
    - clearBoard will take every element and will empty it.
    - reset dashboard to an empty dashBoard

--- assets/scripts/js/play.js
+++ assets/scripts/js/play.js
@@ -1,10 +1,17 @@
 const winningPlays = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [6, 4, 2]]
-const dashBoard = ['', '', '', '', '', '', '', '', '']
-
+let dashBoard = ['', '', '', '', '', '', '', '', '']
+let boardFull = false
 
 let play = true
 const playX = 'X'
 const playO = 'O'
+
+const clearBoard = function () {
+  for (let i = 0; i <= 8; i++) {
+    document.getElementById('c0' + i).innerHTML = ''
+  }
+  dashBoard = ['', '', '', '', '', '', '', '', '']
+}
 
 const placeX = function (x) {
   const id = 'c0' + x
@@ -18,23 +25,32 @@ const placeO = function (O) {
 }
 
 
-const checkWin = function (play) {
-const pushIndex = []
-let arr
-//find all positions and add to the array
-for (let i = 0; i < dashBoard.length; i++) {
+const checkMatch = function (play) {
+  const pushIndex = []
+  let arr
+  // find all positions and add to the array
+  for (let i = 0; i < dashBoard.length; i++) {
     if (dashBoard[i] === play) {
       pushIndex.push(i)
-      //check for winner, compare all the positions to the winnning array if true
+      // check for winner, compare all the positions to the winnning array if true
       // then we found a winner
-       arr = winningPlays.some(a => pushIndex.length === a.length && a.every((v, i) => v === pushIndex[i]))
-
+      arr = winningPlays.some(a => pushIndex.length === a.length && a.every((v, i) => v === pushIndex[i]))
     }
     //  return indexes;
+  }
+  return arr
 }
-  console.log(arr)
-  console.log(pushIndex)
-
+const checkWin = function (play) {
+  const win = checkMatch(play)
+  console.log(win)
+  if (win === true) {
+    document.getElementById('winner').innerText = 'Winner is ' + play
+    boardFull = true
+    // diables clicking event for all divs
+    for (let i = 0; i <= 8; i++) {
+      document.getElementById('c0' + i).style.pointerEvents = 'none'
+    }
+  }
 }
 
 const playedDiv = function (move) {
@@ -43,14 +59,16 @@ const playedDiv = function (move) {
       dashBoard[move] = playX
       play = !play
       placeX(move)
-      checkWin(playX) //look for all X in the array
+      checkWin(playX)
+      // look for all X in the array
       console.log(dashBoard)
     } else if (play === false) {
       dashBoard[move] = playO
       play = !play
       placeO(move)
+      checkWin(playO)
       console.log(dashBoard)
     }
   }
-  //DISABLE DIV TELL USER THAT YOU ALREADY PLAYED THAT
+  // DISABLE DIV TELL USER THAT YOU ALREADY PLAYED THAT
 }
