3e6666fc1c78f7ff183609b5cba64c3d53f3d8b1
jenniferpayano <arlenepayano@gmail.com>
2020-03-28 17:22:50 -0400

    fixed invalid message.
    made tic-tac-toe center.

--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -3,10 +3,11 @@
 const user = {
 }
 const winningPlays = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [6, 4, 2]]
-let dashBoard = ['', '', '', '', '', '', '', '', '']
-let boardFull = false
-let winner = 'X'
-let play = true
+const dashBoard = ['', '', '', '', '', '', '', '', '']
+const boardFull = false
+const winner = 'Draw'
+const won = ''
+const play = true
 const playX = 'X'
 const playO = 'O'
-module.exports = {user, winningPlays, dashBoard, boardFull, play, playX, playO}
+module.exports = {user, winningPlays, dashBoard, boardFull, play, playX, playO, winner, won}
